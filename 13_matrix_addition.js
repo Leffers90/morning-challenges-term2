6d4c7219fd7d7575c2c63268ea75f7d58409e521
Leffers90 <mitchellleffers@gmail.com>
2018-11-26 08:35:52 +1000

    morning challenges, needed alot of help

--- 13_matrix_addition.js
+++ 13_matrix_addition.js
@@ -2,23 +2,127 @@
 Neo is stuck in the matrix and you need to help get him out. In order to
 help him you will need to write a function which will take the values 
 of two different matrices and return their sum total.
-
 Check your solution by running mocha 13_matrix_addition
-
 Examples:
 function matrixAddition ([1,2,3,4], [2,3,4,5]) => [3,5,7,9]
-
 function matrixAddition ([[1,2,3,4], [2,4,6,8]], [[1,2,3,4], [2,4,6,8]])    
                           => [[2,4,6,8], [4,8,12,16]]
-
 Hint:
 It may help to write out the array beforehand if the examples are confusing.
-
 */
+function isValidForAdd(matrixOne, matrixTwo) {
+    // First make sure that we can add these two matrices. If not, return null
+    // First check number of rows in equal
+    if (matrixOne.length === matrixTwo.length) {
+        // Now check number of cols is equal for each row
+        for (let i = 0; i < matrixOne.length; i++) {
+            if (matrixOne[i].length != matrixTwo[i].length) {
+                return false
+            }
+        }
+        //if we get here, number of rows and columns is equal
+        return true
+
+    } else {
+        return false
+    }
+}
+
+function rowAdd(matrixOne, matrixTwo) {
+    return matrixOne.map((val, ind) => val + matrixTwo[ind])
+}
+
+function multiDimensionalAdd(matrixOne, matrixTwo) {
+    let matrixResult = []
+    // Otherwise do the multi-dimensional addition
+    for (let row = 0; row < matrixOne.length; row++) {
+        let matrixRow = rowAdd(matrixOne[row], matrixTwo[row])
+        matrixResult.push(matrixRow)
+    }
+    return matrixResult;
+}
 
 function matrixAddition(matrixOne, matrixTwo) {
+    // Your code here
+    // Check valid matrix sizes
+    if (!isValidForAdd(matrixOne, matrixTwo)) {
+        return null
+    }
+    // Check for single dimension arrays
+    if (matrixOne[0][0] == undefined) {
+        return rowAdd(matrixOne, matrixTwo)
+    } else {
+        return multiDimensionalAdd(matrixOne, matrixTwo)
+    }
 
 }
+
+const assert = require('assert');
+
+describe('Matrix addition', function () {
+    it('Should return the sum of one dimensional matrix', function () {
+        assert.deepEqual([2, 4, 5, 7], matrixAddition([1, 3, 4, 3], [1, 1, 1, 4]))
+        assert.deepEqual([10, 4, 50, 31, 17], matrixAddition([5, 0, 40, 29, 10], [5, 4, 10, 2, 7]))
+        assert.deepEqual([10, 4, 50, 31, 17], matrixAddition([5, 0, 40, 29, 10], [5, 4, 10, 2, 7]))
+    })
+    it('Should return the sum of multidimensional matrix', function () {
+        assert.deepEqual([
+            [2, 4, 6, 8],
+            [10, 12, 14, 16]
+        ], matrixAddition([
+            [1, 2, 3, 4],
+            [5, 6, 7, 8]
+        ], [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8]
+        ]))
+    })
+    it('Should return null if the sum of the matrix cannot be added', function () {
+        assert.deepEqual(null, matrixAddition([4, 2, 1, 7], [2, 3, 5]))
+        assert.deepEqual(null, matrixAddition([8, 12, 5], [40, 21, 8, 17]))
+        assert.deepEqual(null, matrixAddition([
+            [8, 12, 5],
+            [40, 21, 8, 17]
+        ], [
+            [3, 2, 1, 6],
+            [7, 4, 1, 9]
+        ]))
+    })
+})
+
+
+const assert = require('assert');
+
+describe('Matrix addition', function () {
+    it('Should return the sum of one dimensional matrix', function () {
+        assert.deepEqual([2, 4, 5, 7], matrixAddition([1, 3, 4, 3], [1, 1, 1, 4]))
+        assert.deepEqual([10, 4, 50, 31, 17], matrixAddition([5, 0, 40, 29, 10], [5, 4, 10, 2, 7]))
+        assert.deepEqual([10, 4, 50, 31, 17], matrixAddition([5, 0, 40, 29, 10], [5, 4, 10, 2, 7]))
+    })
+    it('Should return the sum of multidimensional matrix', function () {
+        assert.deepEqual([
+            [2, 4, 6, 8],
+            [10, 12, 14, 16]
+        ], matrixAddition([
+            [1, 2, 3, 4],
+            [5, 6, 7, 8]
+        ], [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8]
+        ]))
+    })
+    it('Should return null if the sum of the matrix cannot be added', function () {
+        assert.deepEqual(null, matrixAddition([4, 2, 1, 7], [2, 3, 5]))
+        assert.deepEqual(null, matrixAddition([8, 12, 5], [40, 21, 8, 17]))
+        assert.deepEqual(null, matrixAddition([
+            [8, 12, 5],
+            [40, 21, 8, 17]
+        ], [
+            [3, 2, 1, 6],
+            [7, 4, 1, 9]
+        ]))
+    })
+})
 
 
 
--- 14_multiply_matrices.js
+++ 14_multiply_matrices.js
@@ -2,33 +2,125 @@
 You helped Neo almost get out of the matrix with matrix_addition but
 Cypher was one step ahead of you. In order to get past Cypher's trap and
 save Neo you need to write a new function.
-
 Write a function similar to the matrix_addition challenge but instead
 of addition, write one for multiplication.
-
 Check your solution by running mocha 14_multiply_matrices
-
 Example:
 function matrixMultiplication([[1,2,3],[4,5,6]], [[7,8],[9,10],[11,12]])
 => [[58, 64], [139,154]]
-
 Hint:
 Try drawing the function out first if the arrays are a little confusing.
 */
 
-function multiplyMatrices(matrixOne, matrixTwo) {
-    // Your code here    
+
+// Returns true if the matrix is empty
+function isEmpty(matrix) {
+    return matrix.length == 0
+}
+// Returns true if matrix has a single row
+function isSingleDim(matrix) {
+    return matrix[0][0] == undefined
+}
+// Returns the number of rows in a matrix
+function numRows(matrix) {
+    let rows = 0
+    // if the matrix length is 0, it is empty
+    if (!isEmpty(matrix)) {
+        if (isSingleDim(matrix)) {
+            //single row
+            rows = 1
+        } else {
+            rows = matrix.length
+        }
+    }
+    return rows
+}
+// Returns the number of columns in a matrix
+function numCols(matrix) {
+    let cols = 0
+    if (!isEmpty(matrix)) {
+        if (isSingleDim(matrix)) {
+            cols = matrix.length
+        } else {
+            // Assumes all rows have same number of columns
+            cols = matrix[0].length
+        }
+    }
+    return cols
 }
 
+// Returns true if the number of columns in matrix mOne
+// is equal to the number of rows in matrix mTwo
+function isValidForMultiply(mOne, mTwo) {
+    return numCols(mOne) == numRows(mTwo)
+}
 
-const assert = require ('assert');
+// Returns the column values of a matrix at the given index
+// as an array
+function getCol(matrix, index) {
+    let col = []
+    for (let row of matrix) {
+        col.push(row[index])
+    }
+    return col
+}
+
+// Multiplies one row by one column of two matrices
+// and returns the single dot product element
+function dotProduct(row, col) {
+    let dProd = 0
+    for (let i = 0; i < row.length; i++) {
+        dProd += row[i] * col[i]
+    }
+    return dProd
+}
+// Returns result of matrix multiplication, or null
+// if the matrices cannot be multiplied
+function multiplyMatrices(matrixOne, matrixTwo) {
+    if (!isValidForMultiply(matrixOne, matrixTwo)) {
+        return null
+    }
+    let result = []
+    for (let rowInd = 0; rowInd < matrixOne.length; rowInd++) {
+        let rowResult = []
+        for (let colInd = 0; colInd < matrixTwo[0].length; colInd++) {
+            rowResult.push(dotProduct(matrixOne[rowInd], getCol(matrixTwo, colInd)))
+        }
+        result.push(rowResult)
+    }
+    return result
+}
+
+const assert = require('assert');
 
 describe('Matrix multiplication', function () {
     it('Should return the multipilcation of a matrix', function () {
-        assert.deepEqual([[58,64], [139,154]], multiplyMatrices([[1,2,3],[4,5,6]], [[7,8],[9,10],[11,12]]))
+        assert.deepEqual([
+            [58, 64],
+            [139, 154]
+        ], multiplyMatrices([
+            [1, 2, 3],
+            [4, 5, 6]
+        ], [
+            [7, 8],
+            [9, 10],
+            [11, 12]
+        ]))
     })
-    it('Should return null if the matrix cannot be multiplied', function() {
-        assert.deepEqual(null, multiplyMatrices( [[8,12,5], [40,21,8,17]], [[3,2,1,6], [7,4,1,9]]))
-        assert.deepEqual(null, multiplyMatrices( [[8,12,5], [40,21,8]], [[3,2,1], [7,4,1]]))
+    it('Should return null if the matrix cannot be multiplied', function () {
+        assert.deepEqual(null, multiplyMatrices([
+            [8, 12, 5],
+            [40, 21, 8, 17]
+        ], [
+            [3, 2, 1, 6],
+            [7, 4, 1, 9]
+        ]))
+        assert.deepEqual(null, multiplyMatrices([
+            [8, 12, 5],
+            [40, 21, 8]
+        ], [
+            [3, 2, 1],
+            [7, 4, 1]
+        ]))
     })
 })
